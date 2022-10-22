f09c50adeb5924c8c1b4f37afa7cb1ed85e981db
S.M.Samrat <freefire808709@gmail.com>
2022-10-22 15:40:13 +0600

    change header,,table filter

--- indexApp/views.py
+++ indexApp/views.py
@@ -9,15 +9,14 @@ def Home(request):
     location_filter= request.GET.get('location')
     if location_filter:
         feature_query = Post.objects.filter(post_location__location_name = location_filter)
-        paginator = Paginator(feature_query,10)
+        paginator = Paginator(feature_query,100)
         page_number = request.GET.get('page', 1)
         page_obj = paginator.get_page(page_number)
     else:
         feature_query = Post.objects.all()
-        paginator = Paginator(feature_query,2)
+        paginator = Paginator(feature_query,10)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
-    recent_pro_query = Recently_Properties.objects.all()
     counters = Counters.objects.all()
     agent_query = Agent.objects.all()
     context ={
@@ -25,7 +24,6 @@ def Home(request):
         'feature_details':page_obj,
         'page_number':int(page_number),
         'paginator':paginator,
-        'recent_pro_details':recent_pro_query,
         'counters_details':counters,
         'location_details':location_query,
         'agent_details':agent_query,
@@ -34,11 +32,21 @@ def Home(request):
 
 
 def land_project(request):
-    return render(request,'property/land_project.html')
+    feature_query = Post.objects.all()
+    paginator = Paginator(feature_query,10,orphans=1)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    print(page_obj)
+    context ={
+        'feature_details':page_obj,
+        'page_number':int(page_number),
+        'paginator':paginator,
+    }
+    return render(request,'property/land_project.html',context)
 
 def apartment_project(request):
     feature_query = Post.objects.all()
-    paginator = Paginator(feature_query,6,orphans=1)
+    paginator = Paginator(feature_query,10,orphans=1)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
     print(page_obj)
